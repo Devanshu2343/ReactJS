f06b1019d4fb5e7dbf327cb2e80a323aa567d1e8
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-10-09 00:14:46 +0530

    Create app.js
    using JSX in React

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const NAME = "Dev" ;
+  return (
+    <div>
+      <h1 
+      style ={{
+        color : "red" ,
+        textAlign : "center"
+      }}
+      >
+        Hey , {NAME} !!!!!
+      </h1>
+    </div>
+
+  );
+
+}
+
+export default App;
+
